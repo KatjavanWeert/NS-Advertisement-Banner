fe4971856d0741f1e70f3daca2f8c90686210ed8
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-07-25 17:11:00 +0200

    add GSAP

--- animations.js
+++ animations.js
@@ -1,1 +1,15 @@
 // The people that are displayed can be dependent upon the information that we have about the audience
+
+  const tl = gsap.timeline({ repeatRefresh: true })
+  tl.from(".NSarrow1", {
+    opacity: 0,
+    x: 100,
+  });
+  tl.from(".NSarrow2", {
+    opacity: 0,
+    x: -100,
+  }, "<0.2");
+
+window.addEventListener("click", () => {
+  tl.restart();
+});
