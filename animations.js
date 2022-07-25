eec6981be726f70aa087324ae05a3211aadcc519
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-07-25 19:32:40 +0200

    Animations stage 1 and 2

--- animations.js
+++ animations.js
@@ -1,15 +1,69 @@
 // The people that are displayed can be dependent upon the information that we have about the audience
+// The background image can also be dependent
 
-  const tl = gsap.timeline({ repeatRefresh: true })
-  tl.from(".NSarrow1", {
+const tl1 = gsap.timeline({ repeatRefresh: true });
+
+//Stage 1: introducing NS and grab attention
+tl1.from(".overlay", {
+  opacity: 0,
+});
+tl1.from(".header", {
+  opacity: 0,
+  y: 10,
+});
+tl1.from(
+  ".NSarrow1",
+  {
     opacity: 0,
     x: 100,
-  });
-  tl.from(".NSarrow2", {
+  },
+  "2"
+);
+tl1.from(
+  ".NSarrow2",
+  {
     opacity: 0,
     x: -100,
-  }, "<0.2");
+  },
+  "<0.2"
+);
+
+//Stage 2: open up
+
+tl1.to(
+  ".NSarrow1",
+  {
+    opacity: 0,
+    x: -100,
+  },
+  ">3"
+);
+tl1.to(
+  ".NSarrow2",
+  {
+    opacity: 0,
+    x: 100,
+  },
+  "<"
+);
+tl1.to(".overlay", {
+  opacity: 0,
+});
+tl1.to(
+  ".background",
+  {
+    opacity: 0,
+  },
+  "<0.2"
+);
+tl1.to(
+  ".header",
+  {
+    y: -200,
+  },
+  "<"
+);
 
 window.addEventListener("click", () => {
-  tl.restart();
+  tl1.restart();
 });
